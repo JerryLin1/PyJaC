6350c4c819caad93f0ad69d1609504970eb96431
Jerry Lin <jerrylin9969@gmail.com>
2022-01-18 11:21:32 -0500

    change to function

--- UiPathAuthentication.py
+++ UiPathAuthentication.py
@@ -3,29 +3,37 @@ import requests
 import os
 import json
 
-load_dotenv()
 
-TENANT_NAME = os.getenv("TENANT_NAME")
-CLIENT_ID = os.getenv("CLIENT_ID")
-REFRESH_TOKEN = os.getenv("REFRESH_TOKEN")
+# Returns the the UiPath access token which is needed to make calls to the
+# UiPath Orchestrator API. Expires every 24 hours
+# Requires .env file to be configured with UiPath account tenant info
+def get_uipath_token():
+    load_dotenv()
 
-url = "https://account.uipath.com/oauth/token"
-headers = {
-    "Content-Type": "application/json",
-    "X-UIPATH-TenantName": TENANT_NAME
-}
+    tenant_name = os.getenv("UIPATH_TENANT_NAME")
+    client_id = os.getenv("UIPATH_CLIENT_ID")
+    refresh_token = os.getenv("UIPATH_REFRESH_TOKEN")
 
-data = {
-    "grant_type": "refresh_token",
-    "client_id": CLIENT_ID,
-    "refresh_token": REFRESH_TOKEN
-}
+    url = "https://account.uipath.com/oauth/token"
+    headers = {
+        "Content-Type": "application/json",
+        "X-UIPATH-TenantName": tenant_name
+    }
 
-# For some reason only " works and not ' when making API call
-data = str(data).replace("'", '"')
+    data = {
+        "grant_type": "refresh_token",
+        "client_id": client_id,
+        "refresh_token": refresh_token
+    }
 
-returnValue = requests.post(url, headers=headers, data=data)
+    # For some reason only " works and not ' when making API call
+    data = str(data).replace("'", '"')
 
-authJson = json.loads(returnValue.text)
-print(authJson)
-access_token = authJson['access_token']
+    value = requests.post(url, headers=headers, data=data)
+
+    auth_json = json.loads(value.text)
+    access_token = auth_json['access_token']
+    return access_token
+
+
+print("Access Token is: "+get_uipath_token())
