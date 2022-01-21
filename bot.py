af4d6e03fd5a3785c8bf03990941f3aa3d408fe2
Fernando <fernandomancinidesousa@gmail.com>
2022-01-21 16:24:05 -0500

    Added special case checks

--- bot.py
+++ bot.py
@@ -2,9 +2,7 @@ import os
 import json
 from dotenv import load_dotenv
 from discord.ext import commands
-from forecast import get_forecast
 import UiPathAuthentication
-import time
 
 
 load_dotenv()
@@ -34,11 +32,15 @@ async def send_weather(ctx):
     else:
         temperatures = []
         for city, country in user_cities[ctx.author.name]:
-            temp = get_temperature(ctx.author.name, city, country)
-            temperatures.append(f'{city}, {country}: {temp}' + ':sun_with_face:')
+            temp, warnings = get_temperature(ctx.author.name, city, country)
+            temperatures.append(f'{city}, {country}: {temp}')
             #Record to file
         message = temperatures
     await prev_message.edit(content=f"{message}")
+    if warnings != []:
+        await ctx.send('Warnings:')
+    for warning in warnings:
+        await ctx.send(warning)
 
 
 @bot.command(name="save")
@@ -76,9 +78,12 @@ async def delete_city(ctx, city_name, country_code):
 @bot.command(name="cities")
 async def get_cities(ctx):
     cities = []
-    for city, country in user_cities[ctx.author.name]:
-        cities.append((f'{city}, {country}'))
-    await ctx.send(cities)
+    if cities == []:
+        await ctx.send('You have no saved cities!')
+    else:
+        for city, country in user_cities[ctx.author.name]:
+            cities.append((f'{city}, {country}'))
+        await ctx.send(cities)
 
 def get_temperature(user_name, city, country):
     location = city + ',' + country
@@ -90,7 +95,31 @@ def get_temperature(user_name, city, country):
 
     out = UiPathAuthentication.start_job(ACCESS_TOKEN, input_arguments)
     temp = str(out['main']['temp'])
-    return temp
+    warnings = get_special_cond(temp, out['wind']['speed'], out['main']['humidity'])
+    return (temp, warnings)
+
+def get_special_cond(temp, wind_speed, humidity):
+    humid = humidity >= 65
+    dry = humidity <= 30
+    windy = wind_speed >= 13.5
+    hot = temp >= 35
+    cold = temp <= 0
+    warnings = []
+
+    if humid:
+        warnings.append('-It appears to be a humid day, so make sure to stay hydrated and avoid outdoor activities! :droplet:')
+    if dry:
+        warnings.append('-It appears to be a dry day, so make sure to stay hydrated and avoid outdoor activities and moisturize! :desert:')
+    if hot:
+        warnings.append('-It appears to be a hot day, so make sure to stay hydrated, wear loose clothing, preferably of darker colors! :sun_with_face:')
+    if windy:
+        warnings.append('-Warning :warning: strong winds that may make it hard to walk, and make it colder in low temperatures. :wind_blowing_face:')
+    if cold:
+        warnings.append('-Warning :warning: low temperatures. wear warm clothes, and moisturize! :snowflake:')
+    return warnings
+    
+
+
 
     
 bot.run(TOKEN)
